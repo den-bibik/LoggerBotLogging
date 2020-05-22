3e5f12146154fffc74a521ad88dba15e112bdfcd
Alexander <49481562+WesBAn@users.noreply.github.com>
2020-05-23 00:08:38 +0300

    Fix setup license (#3)

--- setup.py
+++ setup.py
@@ -5,8 +5,16 @@ setup(
     version='0.1',
     packages=[''],
     url='',
-    license='LICENSE',
     author='mc-wesban',
     author_email='wesban1@gmail.com',
-    description='Logger package that can work with LoggerBotLoggingServer'
+    description='Logger package that can work with LoggerBotLoggingServer',
+    classifiers=[
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ]
 )
