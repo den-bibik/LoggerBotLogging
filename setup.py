8213b0273a06bec439f0e277012843adc313a776
Alexander <wesban1@gmail.com>
2020-05-22 20:51:49 +0300

    Create neccessary files for distribution and .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='LoggerBotLogging',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='LICENSE',
+    author='mc-wesban',
+    author_email='wesban1@gmail.com',
+    description='Logger package that can work with LoggerBotLoggingServer'
+)
