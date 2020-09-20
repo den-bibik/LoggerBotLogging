7789e4f81bb505d1da4a97fac9a692705345275f
d.bibik <den-bibik@yandex.ru>
2020-09-21 01:02:18 +0300

    fix sender init

--- bot_logging/sender.py
+++ bot_logging/sender.py
@@ -2,20 +2,10 @@ import requests
 
 
 class ServerSender:
-    def __init__(self, credientals=None):
-        if credientals is not None and "url" in credientals:
-            self.url = credientals["url"]
+    def __init__(self, user_name, user_token, host):
+        self.user_name = user_name
+        self.user_token = user_token
+        self.host = host
 
     def send(self, data):
-
-        r = requests.post(self.url)
-
-    # def prepare_post_json(self, data):
-    #     res = {
-    #         "private_key": self.host_config["private_key"],
-    #         "data": data,
-    #         # TODO: datetime to str/json
-    #         "post_time": datetime.datetime.utcnow(),
-    #         **self._info,
-    #     }
-    #     return res
+        pass
