86018a1d22e42fbd40a0b403f651a1bad6e6b87c
d.bibik <den-bibik@yandex.ru>
2020-09-22 01:15:35 +0300

    add tests.utils

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,34 +1,12 @@
 from unittest import TestCase
 
-from datetime import datetime
 from time import time, sleep
 import concurrent.futures
 
 from bot_logging import RemoteLogger
 from bot_logging.sender import TestSender
 from bot_logging.threading_queue import Singleton
-
-
-def get_logger_funcs(logger):
-    func_dict = {
-        "debug": logger.debug,
-        "info": logger.info,
-        "warning": logger.warning,
-        "error": logger.error,
-        "critical": logger.critical,
-    }
-
-    def get_test_func(key, func):
-        def test_func(postfix):
-            dt = datetime.now()
-            message = "This is a " + key + " message" + postfix
-            func(message)
-            return message, dt
-
-        return test_func
-
-    funcs = [get_test_func(k, func_dict[k]) for k in func_dict]
-    return funcs
+from tests.utils import get_logger_funcs
 
 
 class LoggerLevelTest(TestCase):
